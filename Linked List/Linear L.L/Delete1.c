12c1a1dc291609bc6e7a8b0ab83b054ebd5438ab
JTithy <tithyjebunnesa202@gmail.com>
2025-07-17 10:41:00 +0600

    Delete1.c

---
+++ Linked List/Linear L.L/Delete1.c
@@ -0,0 +1,81 @@
+//Search and delete element at head in a linear linked list 
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+struct Node
+{
+    int data;
+    char data2[100];
+    struct Node *next;
+};
+
+int main()
+{
+    struct Node *head = NULL;
+    struct Node *second = NULL;
+    struct Node *third = NULL;
+
+    head = (struct Node*)malloc(sizeof(struct Node));
+    second = (struct Node*)malloc(sizeof(struct Node));
+    third = (struct Node*)malloc(sizeof(struct Node));
+
+    head->data = 10;
+    strcpy(head->data2, "First");
+    head->next = second;
+
+    second->data = 20;
+    strcpy(second->data2, "Second");
+    second->next = third;
+
+    third->data = 30;
+    strcpy(third->data2, "Third");
+    third->next = NULL;
+
+    struct Node *current = head;
+    if(current == NULL) {
+        printf("The linked list is empty.\n");
+    } else {
+        printf("Linked List: ");
+        while(current != NULL) {
+            printf("%d (%s) -> ", current->data, current->data2);
+            current = current->next;
+        }
+        printf("NULL\n");
+    }
+
+    // Search for an element at the head
+    if (head != NULL) {
+        printf("Head Element: %d, %s\n", head->data, head->data2);
+    } else {
+        printf("The linked list is empty.\n");
+    }
+
+    // Delete the head element
+    if (head != NULL) {
+        struct Node *temp = head;
+        head = head->next;
+        free(temp);
+        printf("Head element deleted.\n");
+    }
+
+    // Print the updated linked list
+    current = head;
+    if(current != NULL)
+    {
+        printf("Updated Linked List: ");
+        while(current != NULL) {
+            printf("%d (%s) -> ", current->data, current->data2);
+            current = current->next;
+        }
+        printf("NULL\n");
+    }
+
+    // Free allocated memory
+    free(head);
+    free(second);
+    free(third);
+
+    return 0;
+}
