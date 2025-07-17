b0e8e7d6fe5dedf461276d38b5595fdfc53baa27
JTithy <tithyjebunnesa202@gmail.com>
2025-07-17 10:54:28 +0600

    Create1.c

---
+++ Linked List/Circular L.L/Circular Singly/Create1.c
@@ -0,0 +1,47 @@
+//Create a Circular Singly linked list using integer data type
+
+#include<stdio.h>
+#include<stdlib.h>
+
+struct Node{
+    int data;
+    struct Node *next;
+};
+
+int main()
+{
+    struct Node *head = NULL;
+    struct Node *second = NULL;
+    struct Node *third = NULL;
+
+    head = (struct Node*)malloc(sizeof(struct Node));
+    second = (struct Node*)malloc(sizeof(struct Node));
+    third = (struct Node*)malloc(sizeof(struct Node));
+
+    head->data = 10;
+    head->next = second;
+
+    second->data = 20;
+    second->next = third;
+
+    third->data = 30;
+    third->next = head;
+
+    struct Node *current = head;
+    if(current == NULL) {
+        printf("The linked list is empty.\n");
+    } else {
+        printf("Linked List: ");
+        while(current != NULL) {
+            printf("%d -> ", current->data);
+            current = current->next;
+        }
+        printf("NULL\n");
+    }
+    
+    free(head);
+    free(second);
+    free(third);
+    
+    return 0;
+}
