0e0090bd326b348355771dcd95c10a0963572928
JTithy <tithyjebunnesa202@gmail.com>
2025-07-19 20:14:49 +0600

    Create3.c

--- Linked List/Doubly L.L/Create3.c
+++ Linked List/Doubly L.L/Create3.c
@@ -1,4 +1,4 @@
-//Create a Doubly linked list using string & integerdata type
+//Create a Doubly linked list using string & integer data type
 
 #include<stdio.h>
 #include<stdlib.h>
@@ -16,42 +16,54 @@ int main()
 
 {
     struct Node *head = NULL;
+    struct Node *first = NULL;
     struct Node *second = NULL;
     struct Node *third = NULL;
 
     head = (struct Node*)malloc(sizeof(struct Node));
+    first = (struct Node*)malloc(sizeof(struct Node));
     second = (struct Node*)malloc(sizeof(struct Node));
     third = (struct Node*)malloc(sizeof(struct Node));
 
-    strcpy(head->name, "Node 1");
-    head->age = 21;
-    head->next = second;
-    head->prev = NULL;
+    head = first;
+
+    strcpy(first->name, "Node 1");
+    first->age = 21;
+    first->next = second;
+    first->prev = NULL;
 
     strcpy(second->name, "Node 2");
     second->age = 22;
     second->next = third;
-    second->prev = head;
+    second->prev = first;
 
     strcpy(third->name, "Node 3");
     third->age = 23;
     third->next = NULL;
     third->prev = second;
 
+    printf("Traversing in forward direction:\n");
     struct Node *current = head;
+
+    printf("NULL -> ");
     while (current != NULL) {
-        printf("Data: %s, Age: %d\n", current->name, current->age);
+        printf("Data: %s, Age: %d -> ", current->name, current->age);
         current = current->next;
     }
     printf("NULL\n");
+
+    printf("Traversing in backward direction:\n");
     current = third;
+    
+    printf("NULL <- ");
     while (current != NULL) {
-        printf("Data: %s, Age: %d\n", current->name, current->age);
+        printf("Data: %s, Age: %d <- ", current->name, current->age);
         current = current->prev;
     }
     printf("NULL\n");
 
     free(head);
+    free(first);
     free(second);
     free(third);
 
