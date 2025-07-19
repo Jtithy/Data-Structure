76f02911ecfd660971f3bfb7009b11ca8b33793d
JTithy <tithyjebunnesa202@gmail.com>
2025-07-19 19:42:08 +0600

    Create1.c

--- Linked List/Doubly L.L/Create1.c
+++ Linked List/Doubly L.L/Create1.c
@@ -14,39 +14,50 @@ int main()
 
 {
     struct Node *head = NULL;
+    struct Node *first = NULL;
     struct Node *second = NULL;
     struct Node *third = NULL;
 
     head = (struct Node*)malloc(sizeof(struct Node));
+    first = (struct Node*)malloc(sizeof(struct Node));
     second = (struct Node*)malloc(sizeof(struct Node));
     third = (struct Node*)malloc(sizeof(struct Node));
 
-    head->data = 10;
-    head->next = second;
-    head->prev = NULL;
+    head->next = first;
+
+    first->data = 10;
+    first->next = second;
+    first->prev = NULL;
 
     second->data = 20;
     second->next = third;
-    second->prev = head;
+    second->prev = first;
 
     third->data = 30;
     third->next = NULL;
     third->prev = second;
 
-    struct Node *current = head;
+    printf("Traversing in forward direction:\n");
+    struct Node *current = first;
+    printf("NULL ->");
+    
     while (current != NULL) {
-        printf("Data: %d\n", current->data);
+        printf(" Data: %d ->", current->data);
         current = current->next;
     }
     printf("NULL\n");
+
+    printf("Traversing in reverse:\n");
     current = third;
+    printf("NULL <-");
     while (current != NULL) {
-        printf("Data: %d\n", current->data);
+        printf(" Data: %d <-", current->data);
         current = current->prev;
     }
     printf("NULL\n");
 
     free(head);
+    free(first);
     free(second);
     free(third);
     
