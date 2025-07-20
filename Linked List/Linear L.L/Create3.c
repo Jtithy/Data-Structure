0a486797c86a997f810dc4218c04b80c8b944d51
JTithy <tithyjebunnesa202@gmail.com>
2025-07-20 08:10:04 +0600

    Create3.c

--- Linked List/Linear L.L/Create3.c
+++ Linked List/Linear L.L/Create3.c
@@ -22,7 +22,7 @@ int main()
     second = (struct Node*)malloc(sizeof(struct Node));
     third = (struct Node*)malloc(sizeof(struct Node));
 
-    head->next = first;
+    head = first;
 
     strcpy(first->name, "Levi Ackerman");
     first->age = 25;
