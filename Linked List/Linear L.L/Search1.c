83447afcaf3d66f03efdaafc484868bf40eadbdc
JTithy <tithyjebunnesa202@gmail.com>
2025-07-20 08:12:07 +0600

    Search1.c

--- Linked List/Linear L.L/Search1.c
+++ Linked List/Linear L.L/Search1.c
@@ -1,4 +1,4 @@
-//Search element at head in a linear linked list 
+//Search element at head in a singly linked list
 
 #include<stdio.h>
 #include<stdlib.h>
@@ -14,16 +14,20 @@ struct Node
 int main()
 {
     struct Node *head = NULL;
+    struct Node *first = NULL;
     struct Node *second = NULL;
     struct Node *third = NULL;
 
     head = (struct Node*)malloc(sizeof(struct Node));
+    first = (struct Node*)malloc(sizeof(struct Node));
     second = (struct Node*)malloc(sizeof(struct Node));
     third = (struct Node*)malloc(sizeof(struct Node));
 
-    head->data = 10;
-    strcpy(head->data2, "First");
-    head->next = second;
+    head = first;
+
+    first->data = 10;
+    strcpy(first->data2, "First");
+    first->next = second;
 
     second->data = 20;
     strcpy(second->data2, "Second");
@@ -54,6 +58,7 @@ int main()
 
     // Free allocated memory
     free(head);
+    free(first);
     free(second);
     free(third);
 
